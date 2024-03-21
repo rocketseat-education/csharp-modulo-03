3df3254f0befc8293781664dc2d7d90f520196a2
Danilo O. Pinheiro <64677271+daniloopinheiro@users.noreply.github.com>
2024-03-21 00:02:21 -0300

    Heranca Entre Classes

--- MyFirstApi/MyFirstApi/Controllers/DeviceController.cs
+++ MyFirstApi/MyFirstApi/Controllers/DeviceController.cs
@@ -2,8 +2,6 @@ namespace MyFirstApi.Controllers;
 
 using Microsoft.AspNetCore.Mvc;
 
-[Route("api/[controller]")]
-[ApiController]
 public class DeviceController : MyFirstApiBaseController
 {
     [HttpGet]
