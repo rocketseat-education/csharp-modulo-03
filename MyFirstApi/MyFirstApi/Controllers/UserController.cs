f3c711af0047582b6a75805eaaa846ff7dd6f4af
Danilo O. Pinheiro <64677271+daniloopinheiro@users.noreply.github.com>
2024-03-21 00:45:51 +0000

    Swagger

--- MyFirstApi/MyFirstApi/Controllers/UserController.cs
+++ MyFirstApi/MyFirstApi/Controllers/UserController.cs
@@ -7,6 +7,8 @@ namespace MyFirstApi.Controllers;
 public class UserController : ControllerBase
 {
     [HttpGet]
+    [ProducesResponseType(typeof(Response), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
     public IActionResult Get()
     {
         var response = new Response
